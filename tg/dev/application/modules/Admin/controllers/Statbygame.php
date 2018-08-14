15d3975cfe5ccc8ff48fa1a511427f599b619b00
liuqi2016 <1551255297@qq.com>
2018-08-14 10:51:12 +0800

    渠道公告

--- tg/dev/application/modules/Admin/controllers/Statbygame.php
+++ tg/dev/application/modules/Admin/controllers/Statbygame.php
@@ -4,18 +4,39 @@ class StatbygameController extends F_Controller_Backend
 {
     protected function beforeList()
     {
-        //游戏统计
+        $m_game = new GameModel();
         $m_pay=new PayModel();
         $m_bygame=new StatbygameModel();
-        $m_game=new GameModel();
         $s = Yaf_Session::getInstance();
         $admin_id=$s->get('admin_id');
         $tg_channel=$admin_id;
+        //统计用户注册数
+//        $signon_people = $m_user->fetch("tg_channel ={$tg_channel}", 'COUNT(user_id) AS reg_people');
+        //统计用户登录次数与人数
+        $m_sign = new SignonlogModel();
+        $peopel_stat=$m_sign->fetchAllBySql("select game_id, COUNT(*) AS signon_times, COUNT(DISTINCT h5.signon_log.user_id) AS signon_people  from h5.signon_log  inner join h5.`user`  on h5.signon_log.user_id = h5.`user`.user_id where h5.`user`.tg_channel =  {$tg_channel}");
+        if($peopel_stat){
+            $conds='';
+            foreach ($peopel_stat as $people){
+                if( $m_bygame->fetch(['game_id'=>$people['game_id'],'admin_id'=>$admin_id])) {
+                    $m_bygame->update($people, $conds."admin_id={$admin_id} AND game_id='{$people['game_id']}'");
+                } else {
+                    $game = $m_game->fetch("game_id='{$people['game_id']}'", 'name,dev_id');
+                    $people['game_name'] = $game['name'];
+                    $people['dev_id'] = $game['dev_id'];
+                    $people['admin_id'] = $admin_id;
+                    $m_bygame->insert($people, false);
+                }
+            }
+        }
+        //游戏统计
         $stat = $m_pay->fetchAll("pay_time > 0 and tg_channel={$tg_channel} and game_id<>0 GROUP BY game_id", 1, 2000000, 'game_id, COUNT(pay_id) AS recharge_times, COUNT(DISTINCT user_id) AS recharge_people, SUM(money) AS recharge_money');
         if( $stat ) {
             $conds='';
+
             foreach ($stat as $row)
             {
+
                 if( $m_bygame->fetch(['game_id'=>$row['game_id'],'admin_id'=>$admin_id])) {
                     $m_bygame->update($row, $conds."admin_id={$admin_id} AND game_id='{$row['game_id']}'");
                 } else {
@@ -24,7 +45,6 @@ class StatbygameController extends F_Controller_Backend
                     $row['dev_id'] = $game['dev_id'];
                     $row['admin_id'] = $admin_id;
                     $m_bygame->insert($row, false);
-                    $arr_bygame[$row['game_id']] = $game['dev_id'];
                 }
             }
         }
