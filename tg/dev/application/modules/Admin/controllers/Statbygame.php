90fee7609f1251223d31427f949c5e8ef7deb758
liuqi2016 <1551255297@qq.com>
2018-08-14 14:05:40 +0800

    渠道公告

--- tg/dev/application/modules/Admin/controllers/Statbygame.php
+++ tg/dev/application/modules/Admin/controllers/Statbygame.php
@@ -7,14 +7,19 @@ class StatbygameController extends F_Controller_Backend
         $m_game = new GameModel();
         $m_pay=new PayModel();
         $m_bygame=new StatbygameModel();
+        $m_sign = new SignonlogModel();
         $s = Yaf_Session::getInstance();
         $admin_id=$s->get('admin_id');
         $tg_channel=$admin_id;
         //统计用户注册数
 //        $signon_people = $m_user->fetch("tg_channel ={$tg_channel}", 'COUNT(user_id) AS reg_people');
         //统计用户登录次数与人数
-        $m_sign = new SignonlogModel();
-        $peopel_stat=$m_sign->fetchAllBySql("select game_id, COUNT(*) AS signon_times, COUNT(DISTINCT h5.signon_log.user_id) AS signon_people  from h5.signon_log  inner join h5.`user`  on h5.signon_log.user_id = h5.`user`.user_id where h5.`user`.tg_channel =  {$tg_channel}");
+        $games=$m_sign->fetchAllBySql("select distinct game_id from h5.signon_log  inner join h5.`user`  on h5.signon_log.user_id = h5.`user`.user_id where h5.`user`.tg_channel =  {$tg_channel}");
+        $peopel_stat=array();
+        foreach ($games as $key=>$game){
+            $game_id=$game['game_id'];
+            $peopel_stat[]=$m_sign->fetchBySql("select game_id, COUNT(*) AS signon_times, COUNT(DISTINCT h5.signon_log.user_id) AS signon_people  from h5.signon_log  inner join h5.`user`  on h5.signon_log.user_id = h5.`user`.user_id where game_id={$game_id} and h5.`user`.tg_channel =  {$tg_channel}");
+        }
         if($peopel_stat){
             $conds='';
             foreach ($peopel_stat as $people){
