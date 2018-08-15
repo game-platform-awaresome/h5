ad724c9535a65559b99d18d26b614643247f6b0b
liuqi2016 <1551255297@qq.com>
2018-08-15 17:56:12 +0800

    redis测试

--- h5/dev/application/controllers/User.php
+++ h5/dev/application/controllers/User.php
@@ -35,7 +35,7 @@ class UserController extends Yaf_Controller_Abstract
     //个人中心主页
     public function indexAction()
     {
-        $ip=$_SERVER['REMOTE_ADDR'];
+        $ip=$this->getIp();
         $host = Yaf_Registry::get('config')->redis->host;
         $port = Yaf_Registry::get('config')->redis->port;
         $conf=array('host'=>$host,'port'=>$port);
@@ -740,7 +740,7 @@ class UserController extends Yaf_Controller_Abstract
 	    $domain_old =isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
         if($domain_old!='h5.zyttx.com'){
             //缓存域名,登录成功后跳转
-            $ip=$_SERVER['REMOTE_ADDR'];
+            $ip=$this->getIp();
             $host = Yaf_Registry::get('config')->redis->host;
             $port = Yaf_Registry::get('config')->redis->port;
             $conf=array('host'=>$host,'port'=>$port);
@@ -1008,4 +1008,29 @@ class UserController extends Yaf_Controller_Abstract
 	    
 	    return false;
 	}
+
+    //不同环境下获取真实的IP
+    function getIp(){
+        //判断服务器是否允许$_SERVER
+        if(isset($_SERVER)){
+            if(isset($_SERVER[HTTP_X_FORWARDED_FOR])){
+                $realip = $_SERVER[HTTP_X_FORWARDED_FOR];
+            }elseif(isset($_SERVER[HTTP_CLIENT_IP])) {
+                $realip = $_SERVER[HTTP_CLIENT_IP];
+            }else{
+                $realip = $_SERVER[REMOTE_ADDR];
+            }
+        }else{
+            //不允许就使用getenv获取
+            if(getenv("HTTP_X_FORWARDED_FOR")){
+                $realip = getenv( "HTTP_X_FORWARDED_FOR");
+            }elseif(getenv("HTTP_CLIENT_IP")) {
+                $realip = getenv("HTTP_CLIENT_IP");
+            }else{
+                $realip = getenv("REMOTE_ADDR");
+            }
+        }
+
+        return $realip;
+    }
 }
