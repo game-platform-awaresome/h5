0f7b908fab07e4e5cf5b9db4c377a68cf2f23281
liuqi2016 <1551255297@qq.com>
2018-08-15 18:01:39 +0800

    redis测试

--- h5/dev/application/controllers/User.php
+++ h5/dev/application/controllers/User.php
@@ -41,7 +41,7 @@ class UserController extends Yaf_Controller_Abstract
         $conf=array('host'=>$host,'port'=>$port);
         $redis=F_Helper_Redis::getInstance($conf);
         if($redis->get('back_url'.$ip)){
-            $domain=$redis->get('back_url');
+            $domain=$redis->get('back_url'.$ip);
             $redis->del('back_url'.$ip);
             $this->redirect('http://'.$domain.'/user/index');
         }
@@ -1011,26 +1011,14 @@ class UserController extends Yaf_Controller_Abstract
 
     //不同环境下获取真实的IP
     function getIp(){
-        //判断服务器是否允许$_SERVER
-        if(isset($_SERVER)){
-            if(isset($_SERVER[HTTP_X_FORWARDED_FOR])){
-                $realip = $_SERVER[HTTP_X_FORWARDED_FOR];
-            }elseif(isset($_SERVER[HTTP_CLIENT_IP])) {
-                $realip = $_SERVER[HTTP_CLIENT_IP];
-            }else{
-                $realip = $_SERVER[REMOTE_ADDR];
-            }
-        }else{
-            //不允许就使用getenv获取
-            if(getenv("HTTP_X_FORWARDED_FOR")){
-                $realip = getenv( "HTTP_X_FORWARDED_FOR");
-            }elseif(getenv("HTTP_CLIENT_IP")) {
-                $realip = getenv("HTTP_CLIENT_IP");
-            }else{
-                $realip = getenv("REMOTE_ADDR");
-            }
-        }
-
-        return $realip;
+        global $ip;
+        if (getenv("HTTP_CLIENT_IP"))
+            $ip = getenv("HTTP_CLIENT_IP");
+        else if(getenv("HTTP_X_FORWARDED_FOR"))
+            $ip = getenv("HTTP_X_FORWARDED_FOR");
+        else if(getenv("REMOTE_ADDR"))
+            $ip = getenv("REMOTE_ADDR");
+        else $ip = "Unknow";
+        return $ip;
     }
 }
