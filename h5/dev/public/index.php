9c3cece594dbd1b2390442d7dfd6682082b2036d
liuqi2016 <1551255297@qq.com>
2018-08-16 10:07:23 +0800

    跨域session

--- h5/dev/public/index.php
+++ h5/dev/public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+ini_set('session.cookie_domain', 'h5.zyttx.com');
 define('APPLICATION_PATH', realpath('../'));
 
 $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
