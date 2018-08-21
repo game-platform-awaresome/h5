61779a6f417602d5656650100bf959e01ea35bad
liuqi2016 <1551255297@qq.com>
2018-08-21 16:52:05 +0800

    登录sdk

--- h5/dev/application/controllers/Sdkapi.php
+++ h5/dev/application/controllers/Sdkapi.php
@@ -20,15 +20,42 @@ class SdkapiController extends Yaf_Controller_Abstract
 //String q_id = data.getString("q_id");
 //String game_id = data.getString("game_id");
 //Login.this.onLogin(user_id, user_name, user_password, q_id, game_id);
-    function login(){
-        $username=$_REQUEST['username'];
-        $password=$_REQUEST['password'];
-        $q_id=$_REQUEST['qid'];
-        $game_id=$_REQUEST['game_id'];
-        $user=new UsersModel();
-        echo json_encode(['code'=>1,'message'=>'登陆成功']);
+    function init(){
+        Yaf_Dispatcher::getInstance()->disableView();
     }
-    function register(){
+    function loginAction(){
+//        $_REQUEST['data']='username%3Dtest%26password%3D123456%26q_id%3Dnull%26game_id%3Dnull';
+        $request=urldecode($_REQUEST['data']);
+        $request=$this->convertUrlQuery($request);
+        $username=$request['username'];
+        $password=$request['password'];
+        // $username='liuqi9';
+        // $password='123456';
+        $m_user=new UsersModel();
+        if($user=$m_user->fetch(['username'=>$username,md5($password)])){
+            $data['status']=100;
+            $info['user_id']=$user['user_id'];
+            $info['user_name']=$user['nickname'];
+            $info['user_password']=$user['password'];
+            $info['q_id']=$request['q_id']??0;
+            $info['game_id']=$request['game_id']??0;
+            $data['info']=$info;
+        }else{
+            $data['msg']='账号或密码错误';
+        }
+        echo json_encode($data);
+    }
+    function registerAction(){
         echo json_encode(['code'=>1,'message'=>'注册成功']);
+    }
+    function convertUrlQuery($query)
+    {
+    $queryParts = explode('&', $query);
+    $params = array();
+    foreach ($queryParts as $param) {
+        $item = explode('=', $param);
+        $params[$item[0]] = $item[1];
+    }
+    return $params;
     }
 }
