3b4beacc3d12781633285acd88d46c4012a4d85d
liuqi2016 <1551255297@qq.com>
2018-08-24 10:48:05 +0800

    渠道sdk分包

--- tg/dev/application/modules/Admin/controllers/Admin.php
+++ tg/dev/application/modules/Admin/controllers/Admin.php
@@ -145,14 +145,14 @@ class AdminController extends F_Controller_Backend
         $game_id=$_GET['game_id']??die('游戏id必须');
         $admin_id=$_SESSION['admin_id'];
         $zip = new ZipArchive();
-        $filename = "/www/wwwroot/code/open/public/game/apk/{$game_id}.apk";//母包位置
+        $filename = "/www/wwwroot/code/h5/open/dev/public/game/apk/{$game_id}.apk";//母包位置
         //复制一份到当前
         //判断游戏目录是否存在
         $path=APPLICATION_PATH."/public/game/apk/{$game_id}";
         if(!is_dir($path)){
             mkdir($path);
         }
-        shell_exec(" cp {$filename}  /www/wwwroot/code/tg/public/game/apk/{$game_id}/{$admin_id}.apk;  > /dev/null 2>&1 &");
+        shell_exec(" cp {$filename}  /www/wwwroot/code/tg/dev/public/game/apk/{$game_id}/{$admin_id}.apk;  > /dev/null 2>&1 &");
         $now_path=$path."/{$admin_id}.apk";
         if ($zip->open($now_path, ZIPARCHIVE::CREATE)!==TRUE) {
             exit("cannot open <$filename> ");
