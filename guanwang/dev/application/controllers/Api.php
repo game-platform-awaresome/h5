eee6c15e040734530eece416ca1e6495dcf02900
liuqi2016 <1551255297@qq.com>
2018-09-29 17:32:16 +0800

    首页图标

--- guanwang/dev/application/controllers/Api.php
+++ guanwang/dev/application/controllers/Api.php
@@ -113,7 +113,7 @@ class ApiController extends Yaf_Controller_Abstract
         $time=date('Y-m-d H:i:s');
         $myfile = fopen("/www2/wwwroot/code/h5/guanwang/dev/public/count.txt", "a") or die("Unable to open file!");
         //w  重写  a追加
-        $txt = 'IP:'.$ip.'@'.$time."\n";
+        $txt = 'IP:'.$ip.'@'.$time.";\n";
         fwrite($myfile, $txt);
     }
     function getIp(){
