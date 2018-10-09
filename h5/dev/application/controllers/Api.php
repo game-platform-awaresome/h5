6b076bb7a6123d06e061a6c96920198ff77e9ea4
liuqi2016 <1551255297@qq.com>
2018-10-09 14:58:04 +0800

    api游戏详情礼包列表

--- h5/dev/application/controllers/Api.php
+++ h5/dev/application/controllers/Api.php
@@ -437,7 +437,20 @@ class ApiController extends Yaf_Controller_Abstract
 
         $game['screenshots'] = empty($game['screenshots']) ? array() : unserialize($game['screenshots']);
         $assign['game'] = $game;
-        exit(json_encode($game));
+        //礼包详情
+        $order = 'h5.giftbag.game_id DESC';
+        $m_gift = new GiftbagModel();
+        $gifts = $m_gift->fetchAllBySql("select h5.giftbag.name,game_name,nums,used,content,gift_id,h5.game.logo from h5.giftbag  inner join h5.`game`  on h5.giftbag.game_id = h5.`game`.game_id where h5.`game`.game_type =  '{$game_type}' order by {$order}  LIMIT {$offset},{$limit} ");
+//        $log="select h5.giftbag.name,game_name,nums,used,content,h5.giftbag.gift_id,h5.game.logo from h5.giftbag  inner join h5.`game`  on h5.giftbag.game_id = h5.`game`.game_id where h5.`game`.game_type =  '{$game_type}' order by {$order}  LIMIT {$offset},{$limit}";
+        foreach ($gifts as &$value){
+            $value['content']=unserialize($value['content']);
+            if($request['user_id']??0) {
+                $rs=$m_gift->fetchBySql("select cdkey from h5.user_cdkey where user_id = {$request['user_id']} and gift_id = {$value['gift_id']}");
+                $value['cdkey'] = $rs['cdkey'];
+            }
+        }
+        $assign['gift'] = $gifts;
+        exit(json_encode($assign));
 //        $m_user = new UsersModel();
 //        $assign['user'] = $m_user->getLogin();
 //        if( $assign['user'] ) {
@@ -531,27 +544,6 @@ class ApiController extends Yaf_Controller_Abstract
                 echo json_encode($rs);
                 die;
             }
-        }
-    }
-    private function downFile($file_name,$file_dir){
-        //检查文件是否存在
-        if (! file_exists ( $file_dir . $file_name )) {
-            header('HTTP/1.1 404 NOT FOUND');
-        } else {
-            //以只读和二进制模式打开文件
-            $file = fopen ( $file_dir . $file_name, "rb" );
-            //告诉浏览器这是一个文件流格式的文件
-            Header ( "Content-type: application/octet-stream" );
-            //请求范围的度量单位
-            Header ( "Accept-Ranges: bytes" );
-            //Content-Length是指定包含于请求或响应中数据的字节长度
-            Header ( "Accept-Length: " . filesize ( $file_dir . $file_name ) );
-            //用来告诉浏览器，文件是可以当做附件被下载，下载后的文件名称为$file_name该变量的值。
-            Header ( "Content-Disposition: attachment; filename=" . $file_name );
-            //读取文件内容并直接输出到浏览器
-            echo fread ( $file, filesize ( $file_dir . $file_name ) );
-            fclose ( $file );
-            exit ();
         }
     }
 }
