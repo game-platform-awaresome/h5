97f9cc8b45deb2a67b5ea2aaee3be846998f06cf
liuqi2016 <1551255297@qq.com>
2018-10-17 17:18:43 +0800

    资讯列表

--- h5/dev/application/controllers/Api.php
+++ h5/dev/application/controllers/Api.php
@@ -291,16 +291,40 @@ class ApiController extends Yaf_Controller_Abstract
         echo json_encode($games, true);
         die;
     }
-
+    function getArticleListAction(){
+        $req = $this->getRequest();
+        if( ! $req->isPost() ) {
+            exit;
+        }
+        $type = $req->getPost('type', '');
+        $type = preg_replace('/[\'\"\\\]+/', '', $type);
+        $pn = $req->getPost('pn', 1);
+        $limit = $req->getPost('limit', 6);
+        $m_article = new ArticleModel();
+        if( $type == '综合' ) {
+            $conds = 'visible=1';
+        } else if( in_array($type, $m_article->_types) ) {
+            $conds = "type='{$type}' AND visible=1";
+        } else {
+            exit;
+        }
+        $conds .= " and type!='代理公告'";//过滤公告
+        $list = $m_article->fetchAll($conds, $pn, $limit, 'article_id,cover,title,up_time', 'weight ASC,article_id DESC');
+        foreach ($list as &$row)
+        {
+            $row['up_time'] = $m_article->formatTime($row['up_time']);
+        }
+        $assignp['list']=$list;
+        $assign['type']=$type;
+        echo json_encode($assign, true);
+        die;
+    }
     /**
      * 获取文章详情
      */
     function getArticleDeatilAction(){
         $request = $_GET;
-        $type = $request['type'];
-        $type = preg_replace('/[\'\"\\\]+/', '', $type);
         $article_id = $request['article_id'];
-        $assign['type'] = $type;
         $m_article = new ArticleModel();
         $assign['info'] = $m_article->fetch("article_id='{$article_id}' AND visible=1");
         echo json_encode($assign, true);
