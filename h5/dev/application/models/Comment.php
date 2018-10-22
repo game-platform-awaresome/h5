54c3f299a06cda278188b8e5123a8fbbd65700fe
liuqi2016 <1551255297@qq.com>
2018-10-22 15:02:09 +0800

    支付统计

--- h5/dev/application/models/Comment.php
+++ h5/dev/application/models/Comment.php
@@ -32,7 +32,7 @@ class CommentModel extends F_Model_Pdo
     {
         $offset = ($pn - 1) * $limit;
         $pdo = $this->getPdo();
-        $stm = $pdo->query("SELECT * FROM comment WHERE game_id='{$game_id}' and  parent_id=0, LIMIT {$offset},{$limit}");
+        $stm = $pdo->query("SELECT * FROM comment WHERE game_id='{$game_id}' and parent_id = 0 LIMIT {$offset},{$limit}");
         $logs = array();
         $row = $stm->fetch(PDO::FETCH_ASSOC);
         while ($row)
