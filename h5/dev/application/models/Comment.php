d3d08e09f132e00ee7b1ccf6ce131633d67cd5c1
liuqi2016 <1551255297@qq.com>
2018-10-22 16:07:04 +0800

    支付统计

--- h5/dev/application/models/Comment.php
+++ h5/dev/application/models/Comment.php
@@ -32,7 +32,8 @@ class CommentModel extends F_Model_Pdo
     {
         $offset = ($pn - 1) * $limit;
         $pdo = $this->getPdo();
-        $stm = $pdo->query("SELECT comment.*,user.username,user.avatar FROM comment LEFT join user on user.user_id=comment.user_id WHERE game_id='{$game_id}' and parent_id = 0 LIMIT {$offset},{$limit}");
+        echo "SELECT comment.*,user.username,user.avatar FROM comment LEFT join user on user.user_id=comment.user_id WHERE game_id='{$game_id}' and parent_id = 0 ORDER BY commit_time desc LIMIT {$offset},{$limit} ";
+        $stm = $pdo->query("SELECT comment.*,user.username,user.avatar FROM comment LEFT join user on user.user_id=comment.user_id WHERE game_id='{$game_id}' and parent_id = 0 ORDER BY comm_time desc LIMIT {$offset},{$limit} ");
         $logs = array();
         $row = $stm->fetch(PDO::FETCH_ASSOC);
         while ($row)
@@ -43,7 +44,7 @@ class CommentModel extends F_Model_Pdo
         //递归
 
         foreach ($logs as $key =>&$value){
-            $value['children']=$this->fetchAllBySql("SELECT comment.*,user.username,user.avatar FROM comment LEFT join user on user.user_id=comment.user_id where parent_id={$value['comm_id']}");
+            $value['children']=$this->fetchAllBySql("SELECT comment.*,user.username,user.avatar FROM comment LEFT join user on user.user_id=comment.user_id where parent_id={$value['comm_id']} ORDER BY comm_time desc");
             //回复人的id
         }
         return $logs;
